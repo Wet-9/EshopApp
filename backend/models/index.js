54e1cd1ce19591399d008eae22e4d19460d3b6fc
Jaydin <jyin.bw@gmail.com>
2023-09-01 15:23:00 -0600

    angular - routes

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,21 +8,17 @@ const User = require('../models/user');
 const db = {};
 
 // one to many relationship
-SubCategory.hasMany(Product, {foreignKey: {name: 'subCategoryId', allowNull: false, onDelte: 'CASCADE', onUpdate: 'CASCADE'}});
-
-Product.belongsTo(SubCategory);
+SubCategory.hasMany(Product, {foreignKey: {name: 'subCategoryId', allowNull: false, onDelete: 'CASCADE', onUpdate: 'CASCADE'}});
 
 //one to one relationship
-Product.hasOne(Inventory, {foreignKey: {name: 'productId', allowNull: false, onDelte: 'CASCADE', onUpdate: 'CASCADE'}});
+Product.hasOne(Inventory, {foreignKey: {name: 'productId', allowNull: false, onDelete: 'CASCADE', onUpdate: 'CASCADE'}});
 
-Inventory.belongsTo(Product);
 // one to many relationship
-Category.hasMany(SubCategory, {foreignKey: {name: 'categoryId', allowNull: false, onDelte: 'CASCADE', onUpdate: 'CASCADE'}});
+Category.hasMany(SubCategory, {foreignKey: {name: 'categoryId', allowNull: false, onDelete: 'CASCADE', onUpdate: 'CASCADE'}});
 
-SubCategory.belongsTo(Category);
 // initialize model
 db.User = User;
-
+    
 db.sequelize = sequelize;
 
 module.exports = db;
