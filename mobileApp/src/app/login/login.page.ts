720e56eef530be94e05fba806820a405a138d6ee
Jaydin <jyin.bw@gmail.com>
2023-09-04 16:20:07 -0600

    fixed bug

--- mobileApp/src/app/login/login.page.ts
+++ mobileApp/src/app/login/login.page.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 
-import { RegisterPageComponent } from `../register`;
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
